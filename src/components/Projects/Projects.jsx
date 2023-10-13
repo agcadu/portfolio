fe359d59d33ee57674ce8fea0ac88847c90e035d
Agustin <agcadu@gmail.com>
2023-10-13 21:02:20 +0200

    añado proyectos

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,6 +7,13 @@ export const Projects = () => {
   return (
     <section className={styles.container} id="projects">
       <h2 className={styles.title}>Proyectos</h2>
+      <p className={styles.description}>
+        Todos los proyectos desarrollados con Spring en este portfolio están
+        dockerizados. Cada proyecto incluye su respectivo Dockerfile y
+        configuración Docker Compose, facilitando así su despliegue y gestión.
+        Además, en casos pertinentes, se proporciona una colección Postman para
+        facilitar las pruebas de API.
+      </p>
       <div className={styles.projects}>
         {projects.map((project, id) => {
           return <ProjectCard key={id} project={project} />;
