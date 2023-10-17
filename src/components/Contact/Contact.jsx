37154cbdfe94a8fee32180ebf2f925ddcf7cd66b
Agustin <agcadu@gmail.com>
2023-10-17 18:22:00 +0200

    modifico datos proyectos

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import { getImageUrl } from '../../utils'
-import styles from './Contact.module.css'
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./Contact.module.css";
 
 export const Contact = () => {
   return (
-    <footer id='contact' className={styles.container}>
-        <div className={styles.text}>            
-            <p>¡PONGÁMONOS EN CONTACTO!</p>
-        </div>
-        <ul className={styles.links}>
-            <li className={styles.link}>
-                <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon"/>
-                <a href="mailto:agcadu@gamil.com">agcadu@gmail.com</a>
-            </li>
-            <li className={styles.link}>
-                <img src={getImageUrl("contact/linkedinIcon.png")} alt="Linkedin icon"/>
-                <a href="https://www.linkeding.com/agcadu">linkeding.com/agcadu</a>
-            </li>
-            <li className={styles.link}>
-                <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon"/>
-                <a href="https://github.com/agcadu">https://github.com/agcadu</a>
-            </li>            
-        </ul>
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <p>¡PONGÁMONOS EN CONTACTO!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:agcadu@gamil.com">agcadu@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="Linkedin icon"
+          />
+          <a href="https://www.linkedin.com/in/agcadu">
+            www.linkedin.com/in/agcadu
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/agcadu">github.com/agcadu</a>
+        </li>
+      </ul>
     </footer>
-  )
-}
+  );
+};
