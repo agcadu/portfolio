585f3a4bafe2d5a6f0d193b10da81caf4947e8d7
Agustin <agcadu@gmail.com>
2023-11-27 21:41:19 +0100

    añado analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import ReactGA from 'react-ga';
+
 import styles from './App.module.css';
 import { Navbar } from './components/Navbar/Navbar';
 import { Header } from './components/Header/Header';
@@ -9,10 +9,7 @@ import { Projects } from './components/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
 
 function App() {
-  useEffect(() => {
-    ReactGA.initialize('G-GCSJTBWKF7');
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+  
 
   return (
     <div className={styles.App}>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ReactGA from "react-ga4";
 
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactGA.initialize("G-CQC1KVG8MV");
+
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
