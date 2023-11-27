376aa40afe0520b6be8e4ffb82527d8828dd1488
Agustin <agcadu@gmail.com>
2023-11-27 20:46:13 +0100

    añado analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import styles from './App.module.css';
 import { Navbar } from './components/Navbar/Navbar';
 import { Header } from './components/Header/Header';
@@ -7,8 +8,11 @@ import { Skills } from './components/Skills/Skills';
 import { Projects } from './components/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
 
-
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-GCSJTBWKF7');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
   return (
     <div className={styles.App}>
@@ -23,4 +27,5 @@ function App() {
 }
 
 export default App;
+
 
